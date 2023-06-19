a1881df545e86d7b40e2a022c367480bd6238167
unknown <vktrysv@gmail.com>
2023-06-19 12:54:32 +0300

    Next time we make the fleet move

--- images/resized_ship_img.py
+++ images/resized_ship_img.py
@@ -25,3 +25,29 @@ for x in range(new_width):
 
 
 image.save('ship_img_small.bmp')
+
+image = Image.open('alienship.bmp')
+
+new_width = 90
+new_height = 70
+
+resized_image = image.resize((new_width, new_height))
+
+resized_image.save('alienship_small.bmp')
+
+
+image = Image.open('alienship_small.bmp')
+image = image.convert('RGBA')
+new_width, new_height = image.size
+background_color = (255, 255, 255)
+
+
+for x in range(new_width):
+    for y in range(new_height):
+        pixel_color = image.getpixel((x,y))
+
+        if pixel_color[:3] == background_color:
+             image.putpixel((x,y), (0, 0, 0, 0))
+
+
+image.save('alienship_small.bmp')
